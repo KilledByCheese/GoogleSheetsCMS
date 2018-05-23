9aa2619e97c923aaec6ac1743a51f33cae0e4c87
KilledByCheese <KToffelROFL@gmail.com>
2018-05-23 15:33:40 +0200

    added css for each category to color code them

--- src/js/main.js
+++ src/js/main.js
@@ -77,11 +77,12 @@ const app = function () {
 	function _renderPosts (posts) {
 		posts.forEach(function (post) {
 			const article = document.createElement('article');
+			const category = 'category_' + post.category;
 			article.innerHTML = `
 				<h2>${post.title}</h2>
 				<div class="article-details">
 					<div>By ${post.author} on ${_formatDate(post.zeitstempel)}</div>
-					<div>Posted in ${post.category}</div>
+					<div class="${category}">Posted in ${post.category}</div>
 				</div>
 				${_formatContent(post.content)}
 			`;
